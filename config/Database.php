eafe3a7dae0d3b716638a04112edef0dd19801c4
Mansi Sen <senm42728@gmail.com>
2023-04-24 12:17:19 +0530

    First Commit

---
+++ config/Database.php
@@ -0,0 +1,42 @@
+<!-- 
+// session_start();
+// class Database{
+
+//     private $host = 'localhost';
+//     private $user = 'root';
+//     private $password = "";
+//     private $database = "attendance_system";
+
+//     public function getConnection(){
+//         $conn = new mysqli($this->host,$this->user,
+//         $this->password, $this->database);
+//         if($conn->connect_error){
+//             die("Error failed to connect to Mysql: " .
+//             $conn->connect_error);
+//         }else {
+//             return $conn;
+//         }
+//     }
+
+// }
+// ?> -->
+
+<?php
+session_start();
+class Database{
+	
+	private $host  = 'localhost';
+    private $user  = 'root';
+    private $password   = "";
+    private $database  = "attendance_system"; 
+    
+    public function getConnection(){		
+		$conn = new mysqli($this->host, $this->user, $this->password, $this->database);
+		if($conn->connect_error){
+			die("Error failed to connect to MySQL: " . $conn->connect_error);
+		} else {
+			return $conn;
+		}
+    }
+}
+?>
---
+++ logout.php
@@ -0,0 +1,9 @@
+<?php 
+session_start();
+$_SESSION["userid"] = '';
+session_destroy();
+header("Location:index.php");
+?>
+
+
+
